386102523693e9f099d9af04b47cd4d3643f1dc7
ListenerJubatus <shawn.thompson306@gmail.com>
2018-03-26 16:53:00 -0600

    minor change

--- Graphics/NoteField layers.lua
+++ Graphics/NoteField layers.lua
@@ -3,12 +3,12 @@ local filter_color= color("0.135,0.135,0.135,1")
 local layers= {
 	Def.Quad{
 		InitCommand= function(self)
-			self:hibernate(math.huge):draworder(notefield_draw_order.board)
+			self:hibernate(math.huge):draworder(newfield_draw_order.board)
 				:diffuse(filter_color)
 		end,
 		PlayerStateSetCommand= function(self, param)
 			local pn= param.PlayerNumber
-			local alf= player_config:get_data(pn).ScreenFilter
+			local alf= getenv("ScreenFilter"..ToEnumShortString(pn)) or 0
 			if alf > 0 then
 				self:SetHeight(4096):diffusealpha(alf):hibernate(0)
 			end
